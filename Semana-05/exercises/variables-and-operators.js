4d376752861c479f204fb180561613b4326c4990
julian lescano <lescano.julian22@gmail.com>
2022-04-16 15:11:13 -0300

    BaSP-2022: variables and operators exercise done

---
+++ Semana-05/exercises/variables-and-operators.js
@@ -0,0 +1,22 @@
+/* Exercise 1: Variables and Operatos*/
+
+//Exercise 1.a 
+
+var number1 = 5;
+var number2 = 10;
+var addition = number1 + number2;
+console.log(addition);
+
+//Exercise 1.b 
+
+var firstName = 'Julian';
+var lastName = 'Lescano';
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+//Exercise 1.c 
+
+var domesticAnimal = 'dog';
+var wildAnimal = "elephant";
+var additionString = domesticAnimal.length + wildAnimal.length;
+console.log(additionString);
